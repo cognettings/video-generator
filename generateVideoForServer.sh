72d3ce74431aa2f30f5ba730042bb5b250ec34c2
John Cognetti <jcognetti@adventive.com>
2022-01-09 21:38:23 -0500

    feat: add video generator server web interface

---
+++ generateVideoForServer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# scrape image urls then download the files
+node imageDownloader.js "$1" $2 &&
+
+# resize the images
+node imageMagick.js "mogrify -format jpg -resize 1280x720 -gravity center -background black -extent 1280x720 ./output/\"$1\"/*.image" &&
+
+# download audio
+# node downloadAudio.js "$1" &&
+curl -d "{\"query\":\"$1\"}" -H "Content-Type: application/json" -X POST http://localhost:3000/main -o output/"$1"/audio &&
+# curl -d "{\"query\":\"cat\"}" -H "Content-Type: application/json" -X POST http://localhost:3000/main -o audio
+
+# create video
+cd output/"$1" &&
+$(node ../../ffmpeg.js) -framerate 1 -pattern_type glob -i '*.jpg' -i audio -map 0:v -map 1:a -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -r 30 "$1".mp4 &&
+# cd - &&
+cd -
+
+# play the video
+# open output/"$1"/"$1".mp4
+
+# curl -d '{"query":"eve"}' -H "Content-Type: application/json" -X POST http://10.0.0.34:3000/main -o "dog"/audio
---
+++ videoGeneratorServer.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const cors = require('cors')
+const exec = require('child_process').exec;
+const app = express()
+const fs = require('fs')
+const port = 3001
+
+// i don't think i need this anymore...i think it was just so i could make a localhost request in the browser
+app.use(cors())
+app.options('*', cors())
+
+app.use(express.static('html'))
+
+app.get('/', (req, res) => {
+  res.send(`Hello World! ${Date.now()}`)
+})
+
+app.get('/video', (req, res) => {
+  const searchTerm = req.query.q
+
+  if (typeof searchTerm === 'undefined') {
+    res.status(400).end()
+  } else {
+    res.sendFile(`./output/${searchTerm}/${searchTerm}.mp4`, { root: __dirname })
+  }
+})
+
+app.get('/generateVideo', (req, res) => {
+  const searchTerm = req.query.q
+  const numberOfImages = req.query.n
+
+  // limit number of images
+  if (parseInt(numberOfImages) > 200) {
+    numberOfImages = 200
+  }
+
+  if (typeof searchTerm === 'undefined' || typeof numberOfImages === 'undefined' || isNaN(numberOfImages)) {
+    res.status(400).end()
+  } else {
+    console.log(searchTerm, numberOfImages)
+
+    if (fs.existsSync(`./output/${searchTerm}/${searchTerm}.mp4`)) {
+      res.sendFile(`./output/${searchTerm}/${searchTerm}.mp4`, { root: __dirname })
+      console.log('sending old file')
+      return
+    } else {
+      console.log('need to create')
+    }
+
+    execute(`npm run generateForServer "${searchTerm}" ${numberOfImages}`, (stdout) => {
+      console.log('done', stdout)
+      res.sendFile(`./output/${searchTerm}/${searchTerm}.mp4`, { root: __dirname })
+      // res.download(`./output/${searchTerm}/${searchTerm}.mp4`)
+    })
+  }
+})
+
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+};
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
