9bcc25af9374a39db23177be750b9a5a6a0fe2c6
John Cognetti <jcognetti@adventive.com>
2022-11-04 17:27:22 -0400

    fix: DATADIR not set for webapp

--- generateVideoForServer.sh
+++ generateVideoForServer.sh
@@ -6,12 +6,12 @@ node thumbnailImageDownloader.js "$1" $2 &&
 # resize the images
 echo converting the images &&
 # node imageMagick.js "mogrify -format jpg -resize 1280x720 -gravity center -background black -extent 1280x720 ./output/\"$1\"/*.image" &&
-node resizeImages.js "./output/$1/"
+node resizeImages.js "$1/" &&
 
 # download audio
 # node downloadAudio.js "$1" &&
 echo downloading the audio &&
-curl -d "{\"query\":\"$1\"}" -H "Content-Type: application/json" -X POST http://localhost:3000/main -o output/"$1"/audio &&
+curl -d "{\"query\":\"$1\"}" -H "Content-Type: application/json" -X POST http://localhost:3000/main -o $DATADIR"$1"/audio &&
 # curl -d "{\"query\":\"cat\"}" -H "Content-Type: application/json" -X POST http://localhost:3000/main -o audio
 
 # create video
