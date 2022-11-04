2b66ace1ae6de91628a508ce3499eb067efe59b6
John Cognetti <jcognetti@adventive.com>
2022-11-04 00:12:32 -0400

    feat: generateVideo returns readable stream

--- generateVideo.js
+++ generateVideo.js
@@ -4,7 +4,8 @@ const thumbnailImageDownloader = require('./thumbnailImageDownloader.js')
 const resizeImages = require('./resizeImages.js')
 const ffmpeg = require('./ffmpeg')
 const downloadSong = require('./SongDownloader/downloadSong.js')
-const fs = require('node:fs/promises')
+const fsp = require('node:fs/promises')
+const fs = require('fs')
 
 module.exports = generateVideo
 
@@ -29,6 +30,7 @@ async function generateVideo (topic, numberOfImages = 10, duration = 60) {
   try {
     // console.log(await ffmpeg(`-framerate 1 -pattern_type glob -i './output/${topic}/*.jpg' -i './output/${topic}/${topic}.mp3' -map 0:v -map 1:a -c:v libx264 -c:a aac -pix_fmt yuv420p -profile:v baseline -level 3 -r 30 -t 60.0 './output/${topic}/${topic}.mp4'`))
     console.log(await ffmpeg(`-y -f concat -safe 0 -i '${process.env.DATADIR}${topic}/input.txt' -i '${process.env.DATADIR}${topic}/${topic}.mp3' -map 0:v -map 1:a -c:v libx264 -c:a aac -pix_fmt yuv420p -profile:v baseline -level 3 -r 30 -t ${duration}.0 '${process.env.DATADIR}${topic}/${topic}.mp4'`))
+    return fs.createReadStream(`${process.env.DATADIR}${topic}/${topic}.mp4`)
   } catch (e) {
     console.log(e)
   }
@@ -45,5 +47,5 @@ async function generateInputFile (topic, numberOfImages, duration) {
     imageNumber = ++imageNumber % numberOfImages
   }
 
-  await fs.writeFile(`${process.env.DATADIR}${topic}/input.txt`, fileLines.join('\n'))
+  await fsp.writeFile(`${process.env.DATADIR}${topic}/input.txt`, fileLines.join('\n'))
 }
